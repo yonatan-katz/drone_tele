b0b4a2695d61ebcfa6c65de2d8bcfd8f750c24c5
Yonatan Katz <yonic2006@gmail.com>
2018-02-10 21:24:19 +0200

    Add thread management to the host tele server

--- host/tele_server/server.py
+++ host/tele_server/server.py
@@ -9,9 +9,20 @@ Created on Fri Feb  9 15:24:22 2018
 import socket
 import sys
 import serial
+import threading
+import copy
 
 BIND_IP = "192.168.14.62"
 BIND_PORT = 6060
+
+STOP_EVENT = threading.Event()
+
+
+Q_LOCK = threading.Lock()
+QUATERNION = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]#height and Quaternion a,b,c,d
+
+J_LOCK = threading.Lock()
+JOYSTICK_STATE = [0.0,0.0,0.0,0.0] #gas vertical and horizontal, pos vertical and horizontal
 
 def handle_udp_tele_message(data):
     teapotPacket  = []
@@ -23,40 +34,41 @@ def handle_udp_tele_message(data):
     q[2] = ((teapotPacket[6] << 8) | teapotPacket[7]) / 16384.0
     q[3] = ((teapotPacket[8] << 8) | teapotPacket[9]) / 16384.0
     height = teapotPacket[10] * 5
+    
+    
+    Q_LOCK.acquire()
+    QUATERNION[0] = height
     for i in range(4):
         if (q[i] >= 2):
             q[i] = -4 + q[i]
-
-    print("Height:{}".format(height))
-    print("q[0]:{}".format(q[0]))
-    print("q[1]:{}".format(q[1]))
-    print("q[2]:{}".format(q[2]))
-    print("q[3]:{}".format(q[3]))
-    
-    
-
         
+        QUATERNION[i+1] = q[i]
+    
+    Q_LOCK.release()        
 
 
-def udp_channel():
+def udp_channel(bind_ip,bind_port):
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Bind the socket to the port
-    server_address = (BIND_IP, BIND_PORT)    
+    server_address = (bind_ip, bind_port)    
     sock.bind(server_address)
     
-    while True:
+    while not STOP_EVENT.is_set():
         data, address = sock.recvfrom(4096)
-        print("Got data len:{}".format(len(data)),data.__class__)
+        #print("Got data len:{}".format(len(data)),data.__class__)
         handle_udp_tele_message(data)
         
 
 def handle_serial_message(data):
-    print(data)
+    J_LOCK.acquire()
+    for i in xrange(len(data)):
+        JOYSTICK_STATE[i] = data[i]
+    J_LOCK.release()
         
         
-def serial_channel(port="/dev/ttyUSB0"):
+def serial_channel(port):
     
     ser = serial.Serial()
     ser.port = port
@@ -67,20 +79,20 @@ def serial_channel(port="/dev/ttyUSB0"):
     ser.timeout = None          #block read        
     ser.xonxoff = False     #disable software flow control
     ser.rtscts = False     #disable hardware (RTS/CTS) flow control
-    ser.dsrdtr = False    #disable hardware (DSR/DTR) flow control  
-    
+    ser.dsrdtr = False    #disable hardware (DSR/DTR) flow control
 
 
     try: 
         ser.open()
     except Exception, e:
             print "error open serial port: " + str(e)
-            exit() 
-    DOLLAR=36    
+            exit(1) 
+    
+    DOLLAR=36
     state = "start"
     D = []
-    while True:        
-        ch = ord(ser.read(1))       
+    while not STOP_EVENT.is_set():        
+        ch = ord(ser.read(1))        
         if ch==DOLLAR:
             state = "$"        
         elif ch==2 and state == "$":
@@ -91,18 +103,42 @@ def serial_channel(port="/dev/ttyUSB0"):
             if len(D) == 4:
                 handle_serial_message(D)
                 D = []
-                state = "start"
+                state = "start"    
         
-            
         
-                
-          
         
+if __name__=="__main__":    
+    from time import sleep
     
+    #serial_channel()
+    serial_tele = threading.Thread(target=serial_channel, args=("/dev/ttyUSB0",))
+    udp_tele = threading.Thread(target=udp_channel, args=(BIND_IP,BIND_PORT))
+    
+    serial_tele.setDaemon(True)
+    serial_tele.start()
+    
+    udp_tele.setDaemon(True)
+    udp_tele.start()
+    
+    j = []
+    q = []
+    try:
+        while True:
+            Q_LOCK.acquire()
+            q = copy.copy(QUATERNION)
+            Q_LOCK.release()
+            
+            J_LOCK.acquire()
+            j = copy.copy(JOYSTICK_STATE)
+            J_LOCK.release()
+            sleep(0.02) #sleep 20 miliseconds
+            
+            print(QUATERNION)
+            print(JOYSTICK_STATE)
+    except:
+        global STOP_EVENT
+        print("Stop working threads!")
+        STOP_EVENT.set()
         
         
-        
-        
-if __name__=="__main__":
-    serial_channel()
     
