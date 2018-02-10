c6f9527fdae3157f1c21d11ccb799dc34c86673d
Yonatan Katz <yonic2006@gmail.com>
2018-02-10 20:00:30 +0200

    Fix joystick output range

--- drone/joystick_controller_ino/joystick_controller_ino.ino
+++ drone/joystick_controller_ino/joystick_controller_ino.ino
@@ -1,9 +1,9 @@
 
-//black GND,white 3.3V 
-int gas_vertical = 0;     // green
-int gas_horizont = 1;     // yellow
-int pos_vertical = 2;     // grey
-int pos_horizont = 3;     // white
+
+int gas_vertical = 0;     // potentiometer wiper (middle terminal) connected to analog pin 3
+int pos_vertical = 1;     // potentiometer wiper (middle terminal) connected to analog pin 3
+int gas_horizont = 2;     // potentiometer wiper (middle terminal) connected to analog pin 3
+int pos_horizont = 3;     // potentiometer wiper (middle terminal) connected to analog pin 3
                          
 char teapotPacket[10] = { '$', 0x02, 0, 0, 0, 0, 0x00, 0x00,'\r', '\n' };
 
@@ -14,19 +14,18 @@ void setup()
 
 void loop()
 {
-  /*
-  teapotPacket[2] = analogRead(gas_vertical);     // read the input pin
-  teapotPacket[3] = analogRead(pos_vertical);     // read the input pin
-  teapotPacket[4] = analogRead(gas_horizont);     // read the input pin
-  teapotPacket[5] = analogRead(pos_horizont);     // read the input pin
-  Serial.write(teapotPacket, 10); 
-  teapotPacket[12]++;
-  Serial.write()
-  */
 
-  Serial.print(analogRead(gas_vertical));Serial.print(",");
-  Serial.print(analogRead(gas_horizont));Serial.print(",");
-  Serial.print(analogRead(pos_vertical));Serial.print(",");
-  Serial.print(analogRead(pos_horizont));Serial.print(",");
-  Serial.println("");
+  teapotPacket[2] = (uint8_t)(analogRead(gas_vertical)/3.0);     // read the input pin
+  teapotPacket[3] = (uint8_t)(analogRead(pos_vertical)/3.0);     // read the input pin
+  teapotPacket[4] = (uint8_t)(analogRead(gas_horizont)/3.0);     // read the input pin
+  teapotPacket[5] = (uint8_t)(analogRead(pos_horizont)/3.0);     // read the input pin
+  Serial.write((const uint8_t*)teapotPacket, 10); 
+  teapotPacket[7]++;
+
+#ifdef DEBUG
+  Serial.println(analogRead(gas_vertical));
+  Serial.println(analogRead(pos_vertical));
+  Serial.println(analogRead(gas_horizont));
+  Serial.println(analogRead(pos_horizont));
+#endif
 }
